e865b12f0a4f3aec491e8a63c1088560ee745667
edweltoncslima <edweltonchristian.ti@gmail.com>
2025-01-22 17:45:26 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -2,13 +2,13 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+file_name = 'teste.txt.pegadinhadoveinho'
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
 ## chave para descriptografia
-key = b"testeransomwares"
+key = b'testeransomwares'
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
